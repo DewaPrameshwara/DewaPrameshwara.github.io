3ba0e15c5e7fd12829a5c1c8303e7f17a3a699c2
DePram <dprameshwara@gmail.com>
2024-08-14 16:44:12 +0800

    membuat animasi navbar

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 gsap.registerPlugin(TextPlugin);
 gsap.to(".lead", { duration: 2, delay: 1, text: "Student | Ordinary Person" });
+
+const background = document.querySelector("#back-nav");
+
+const handleScroll = () => {
+  const { scrollY, innerHeight } = window;
+
+  const currentIndex = Math.floor(scrollY / (innerHeight - 200));
+
+  background.style.transform = `translateY(${currentIndex > 0 ? 75 * currentIndex : 0}px) translate(6px, 85px)`;
+};
+
+document.addEventListener("scroll", handleScroll, { passive: true });
