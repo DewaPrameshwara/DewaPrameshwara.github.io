9e192aa9a21d703ddd746f1a8a9aba18fdc82445
DePram <dprameshwara@gmail.com>
2024-08-18 15:13:46 +0800

    menyelesaikan contact section (finish)

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbyD_xi757IjUEU_t-v42Mzl4CrJcqMHzI2InAkPbp9zp7Xg_DFxvVJ_Gyqx2Jr7Zm4/exec";
+const form = document.forms["dode-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // saat tombol submit di klik
+  // tampilkan tombol loading dan hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // saat tombol submit di klik
+      // tampilkan tombol kirim dan hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
 gsap.registerPlugin(TextPlugin);
 gsap.to(".lead", { duration: 2, delay: 1, text: "Student | Ordinary Person" });
 
